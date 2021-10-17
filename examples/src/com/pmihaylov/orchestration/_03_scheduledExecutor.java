a65f171c7b694e3e96d0e41ec577287439576b16
preslavmihaylov <preslav@pmihaylov.com>
2021-10-17 12:43:26 +0300

    enhance example

--- examples/src/com/pmihaylov/orchestration/_03_scheduledExecutor.java
+++ examples/src/com/pmihaylov/orchestration/_03_scheduledExecutor.java
@@ -9,6 +9,39 @@ import static com.pmihaylov.Common.ignoreCheckedExceptions;
 
 public class _03_scheduledExecutor {
     public static void main(String[] args) {
+        inhouseExample();
+        executorServiceExample();
+    }
+
+    public static void inhouseExample() {
+        ScheduledExecutorService exec = Executors.newScheduledThreadPool(10);
+
+        AtomicInteger cnt1 = new AtomicInteger(0);
+        exec.submit(() -> {
+            while (true) {
+                System.out.printf("Printing from thread %s: %d\n", Thread.currentThread().getName(), cnt1.get());
+                cnt1.incrementAndGet();
+                Thread.sleep(500);
+            }
+        });
+
+        AtomicInteger cnt2 = new AtomicInteger(0);
+        exec.submit(() -> {
+            while (true) {
+                System.out.printf("Printing from thread %s: %d\n", Thread.currentThread().getName(), cnt2.get());
+                cnt2.incrementAndGet();
+                Thread.sleep(500);
+            }
+        });
+
+        ignoreCheckedExceptions(() -> Thread.sleep(2000));
+        System.out.println("shutting down executor...");
+
+        exec.shutdownNow();
+        ignoreCheckedExceptions(() -> exec.awaitTermination(5, TimeUnit.SECONDS));
+    }
+
+    public static void executorServiceExample() {
         ScheduledExecutorService exec = Executors.newScheduledThreadPool(10);
 
         AtomicInteger cnt1 = new AtomicInteger(0);
